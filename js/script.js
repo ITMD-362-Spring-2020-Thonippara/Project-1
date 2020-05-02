2edd162e219bc0893d0f90c1d06f681b63198f99
ythonippara <47587277+ythonippara@users.noreply.github.com>
2020-05-02 01:36:44 -0500

    Adding init function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const floatField = document.getElementById('floatField');
-const floatContainer = document.getElementById('floatContainer');
-floatField.addEventListener('focus', () => {
-  floatContainer.classList.add('active');
-});
-floatField.addEventListener('blur', () => {
-  floatContainer.classList.remove('active');
-});
+function init() {
+  const floatField = document.getElementById('float_field');
+  const floatContainer = document.getElementById('float_container');
+  floatField.addEventListener('focus', () => {
+    floatContainer.classList.add('active');
+  });
+  floatField.addEventListener('blur', () => {
+    floatContainer.classList.remove('active');
+  });
+}
+window.addEventListener('load', init);
